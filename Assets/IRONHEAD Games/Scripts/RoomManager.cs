ad1d9ceeb07d1b3e2b9d7dd86c69de7ed75dd94b
DESKTOP-1VLO2MR\MIHAIL <the_mihail@yahoo.com>
2021-10-25 22:54:17 +0300

    Joining Randoom Rooms

---
+++ Assets/IRONHEAD Games/Scripts/RoomManager.cs
@@ -0,0 +1,64 @@
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region UI Callback Methods
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    #endregion
+
+    #region Photon Callback Methods
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("A room is created with the name: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("The Local player: " + PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + 
+            " Player count" + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to: " + "Player count: " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+    #endregion
+
+    #region Private Methods
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room_" + UnityEngine.Random.Range(0, 10000);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 20;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+    #endregion
+}
